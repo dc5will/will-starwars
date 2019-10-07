ff27932743d1e6d9e5f9af64e3ab82e6fce902ba
William Y Wong <wwong1987@gmail.com>
2019-10-07 13:07:30 -0700

    added error handling for height and weight when fetch request returns NaN

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,11 +1,17 @@
 const Helpers = {
   convertCmToInches(cm) {
+    if (isNaN(cm)) {
+      return 'unknown'
+    }
     let realFeet = ((cm*0.393700) / 12);
     let feet = Math.floor(realFeet);
     let inches = Math.round((realFeet - feet) * 12);
     return feet + ' ft ' + inches + ' inches';
   },
   convertKgToLbs(kg) {
+    if (isNaN(kg)) {
+      return 'unknown'
+    }
     return Math.round(kg * 2.2) + ' lbs';
   }
 }
